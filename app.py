93fbc647c93bd8ef47bb98168a05a94688dedaa8
Diegobenitez94 <diego.benitez.9045@miremington.edu.co>
2025-06-10 18:54:19 -0500

    musica_api

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     
     # TODO: Determinar si se debe usar modo debug
-    debug_mode = os.environ.get("FLASK_DEBUG", "False").lower() == "true"
+    debug_mode = os.environ.get ("FLASK_DEBUG", "False").lower() == "true"
     
     # TODO: Ejecutar aplicación
     app.run(host="0.0.0.0", port=port, debug=debug_mode)
